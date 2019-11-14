63c7b5484ce580d612f2521cddded3511482a2a2
jyahn <jooyoungxahn@gmail.com>
2019-11-14 14:01:29 -0800

    added routes for threads and dms

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 
-from flask import Flask, render_template, request, flash, redirect, session, g
-# from flask_debugtoolbar import DebugToolbarExtension
+from flask import Flask, render_template, request, flash, redirect, session, g, jsonify
+from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
 
 from forms import UserAddForm, LoginForm, MessageForm, EditProfileForm, LikesForm
@@ -384,6 +384,78 @@ def homepage():
 
     else:
         return render_template('home-anon.html')
+
+
+##############################################################################
+# Thread and DM pages
+
+
+@app.route('/threads')
+def list_threads():
+    """Page with listing of threads.
+    """
+    # query the threads where I'm user 1
+    my_user1_threads = Thread.query.filter(Thread.user1_id == g.user.id).all()
+    # query the threads where I'm user 2
+    my_user2_threads = Thread.query.filter(Thread.user2_id == g.user.id).all()
+    return render_template('threads.html', my_user1_threads=my_user1_threads, my_user2_threads=my_user2_threads)
+    
+
+@app.route('/threads/add/<int:user_id>', methods=['POST'])
+def add_thread(user_id):
+    """Page to add a thread. """
+    # if this user id combo exists
+    thread = Thread.query.filter(
+        Thread.user1_id == user_id, Thread.user2_id == g.user.id).all()
+
+    thread2 = Thread.query.filter(
+        Thread.user2_id == user_id, Thread.user1_id == g.user.id).all()
+
+    if thread:
+        return redirect(f'threads/{thread[0].id}')
+
+    if thread2:
+        return redirect(f'threads/{thread2[0].id}')
+
+    # else:
+    if (user_id < g.user.id):
+        new_thread = Thread(user1_id=user_id, user2_id=g.user.id)
+    else:
+        new_thread = Thread(user1_id=g.user.id, user2_id=user_id)
+    db.session.add(new_thread)
+    db.session.commit()
+    return redirect(f'threads/{new_thread.id}')
+
+
+@app.route('/threads/<int:thread_id>')
+def show_thread(thread_id):
+    """Page to see a thread. """
+    thread = Thread.query.get(thread_id)
+    if g.user.id == thread.user1_id or g.user.id == thread.user2_id:
+        if (g.user.id == thread.user1_id):
+            other_username = thread.user2.username
+        else:
+            other_username = thread.user1.username
+
+        return render_template("show-thread.html", thread=thread, other_username=other_username)
+    else:
+        flash('Unauthorized', 'danger')
+        return redirect('/')
+
+
+@app.route('/threads/<int:thread_id>/dm/add', methods=["POST"])
+def add_dm(thread_id):
+    """adds a dm"""
+    thread = Thread.query.get(thread_id)
+    text = request.json["text"]
+    dm = DM(text=text, thread_id=thread_id, author=g.user.id)
+    db.session.add(dm)
+    db.session.commit()
+    all_dms = [[dm.text, dm.author] for dm in thread.dms]
+    return jsonify(all_dms)
+
+
+
 
 
 ##############################################################################
