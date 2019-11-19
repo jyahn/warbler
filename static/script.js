d77804d46f1862b469faa72f38ea93fa536640a1
jyahn <jooyoungxahn@gmail.com>
2019-11-19 09:17:32 -0800

    heroku base url change

--- static/script.js
+++ static/script.js
@@ -1,12 +1,14 @@
-// const BASE_URL = 'https://warbler-vsha2.herokuapp.com';
-const BASE_URL = 'http://localhost:5000';
+const BASE_URL = 'https://warbler-ja.herokuapp.com';
+// const BASE_URL = 'http://localhost:5000';
 
 $(document).ready(function () {
   $('#dm-form').on('submit', function (evt) {
     evt.preventDefault();
     let text = $('#dm-input').val();
+    console.log("text is ", text);
     if (text.length > 1) {
       let conversationArr = document.URL.split('/');
+      console.log(conversationArr);
       let conversationId = parseInt(conversationArr[conversationArr.length - 1]);
       $('#dm-form').trigger('reset');
       addDM(text, conversationId, function (resp) {
@@ -17,6 +19,7 @@ $(document).ready(function () {
 });
 
 function addDM(text, conversationId, cb) {
+  console.log("YES")
   $.ajax({
     method: 'POST',
     url: `${BASE_URL}/conversations/${conversationId}/dm/add`,
